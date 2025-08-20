0642871ea534da27449143d44bdf2d6d21bfebff
sami002khan <147750376+sami002khan@users.noreply.github.com>
2025-08-20 17:05:37 +0000

    DataTypes.js

---
+++ DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+// alert( 3+3)  // we are using nodejs, not browser
+
+let age = 21
+console.log(typeof age)
+let isLoggedIn =false
+let state;
+
+
+console.log(3+3)
+console.log("khan")
+
+// bigint
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+ // symbol => unique
+
+console.log(typeof khan)
+
+
+console.log(typeof null)
+console.log(typeof undefined)
